ee8f40fe1f41278fa9afa5c3122306d4b5170659
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2024-03-06 12:46:52 -0300

    configurando cookies

--- api/src/controllers/SessionsController.js
+++ api/src/controllers/SessionsController.js
@@ -27,7 +27,16 @@ class SessionsController {
       expiresIn,
     });
 
-    response.status(201).json({ token, user });
+    response.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "none",
+      secure: true,
+      maxAge: 15 * 60 * 1000,
+    });
+
+    delete user.password;
+
+    response.status(201).json({ user });
   }
 }
 
--- api/src/server.js
+++ api/src/server.js
@@ -3,31 +3,36 @@ require("express-async-errors");
 const cors = require("cors");
 const express = require("express");
 const routes = require("./routes");
-
+const cookieParser = require("cookie-parser");
 const AppError = require("./utils/AppError");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    credentials: true,
+  })
+);
 
 app.use(routes);
 
 app.use((err, request, response, next) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        status: "error",
-        message: err.message,
-      });
-    }
-  
-    console.error(err);
-  
-    return response.status(500).json({
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
       status: "error",
-      message: "Internal server error",
+      message: err.message,
     });
-});
+  }
 
+  console.error(err);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+});
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
